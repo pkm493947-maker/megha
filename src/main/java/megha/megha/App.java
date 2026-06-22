cde57bfeaad7b0c0d1edfa8c49593e5de2fc6256
pkm493947-maker <pkm493947@gmail.com>
2026-06-22 21:34:06 +0530

    first commit

---
+++ src/main/java/megha/megha/App.java
@@ -0,0 +1,13 @@
+package megha.megha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! to git" );
+    }
+}
